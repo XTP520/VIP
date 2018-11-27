e91ab6267413c7faee30a8996d24c61f5f9dce80
Sunlight <1365814172@qq.com>
2018-11-27 10:32:35 +0800

    AppInfoSys-添加了一个JSP。

--- src/cn/appsys/controller/developer/DevUserController.java
+++ src/cn/appsys/controller/developer/DevUserController.java
@@ -1,11 +1,15 @@
 package cn.appsys.controller.developer;
 
+import cn.appsys.pojo.DevUser;
 import cn.appsys.service.developer.DevUserService;
+import cn.appsys.tools.Constants;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,6 +28,41 @@ public class DevUserController {
     @RequestMapping(value = "/login")
     public String login(){
         logger.debug("Controller===================>");
+        return "devlogin";
+    }
+    //
+    @RequestMapping(value = "/dologin")
+    public String dologin(@RequestParam String devCode,
+                          @RequestParam String devpassword,
+                          HttpSession session,
+                          HttpServletRequest servletRequest){
+        logger.debug("dologin===================>");
+        DevUser user = null;
+        //调用Service方法，用户匹配
+        user = service.login(devCode ,devpassword);
+        if (null != user){
+            //放入session
+            session.setAttribute(Constants.DEV_USER_SESSION,user);
+            //页面跳转main.jsp,提示信息
+            return "redirect:/dev/flatform/main";
+        }else {
+            //保留在devlogin页面
+            servletRequest.setAttribute("error","用户名或密码不正确");
+            return "devlogin";
+        }
+    }
+
+    @RequestMapping(value = "/flatform/main")
+    public String main(HttpSession session){
+        if(session.getAttribute(Constants.DEV_USER_SESSION) == null){
+            return "redirect:/dev/login";
+        }
+        return "developer/main";
+    }
+    @RequestMapping(value = "/logout")
+    public String logout(HttpSession session){
+        //清除session
+        session.removeAttribute(Constants.DEV_USER_SESSION);
         return "devlogin";
     }
 }
